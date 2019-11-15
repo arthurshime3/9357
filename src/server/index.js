1c8788f9bc398fa2f9b03a4e86850f97121c851f
Kunal Patel <kunalp1025@gmail.com>
2019-11-15 14:34:28 -0500

    add body-parser to read request body

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('*', function(req, res) {
     res.sendfile('./dist/index.html');
