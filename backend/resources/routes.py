7289d4cd5db5630a1726fd81d25d685f4a1c9951
Kunal Patel <kunalp1025@gmail.com>
2020-01-27 11:14:51 -0500

    change auth to return jwt inside of a httponly cookie

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,6 +1,7 @@
-from .auth import RegistrationApi, LoginApi
+from .auth import RegistrationApi, LoginApi, LogoutApi
 
 
 def initialize_routes(api):
     api.add_resource(RegistrationApi, '/api/register')
     api.add_resource(LoginApi, '/api/login')
+    api.add_resource(LogoutApi, '/api/logout')
