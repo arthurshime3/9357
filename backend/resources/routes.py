25f382a90aa23280c43c8324f2058ddc3ed69a60
Kunal Patel <kunalp1025@gmail.com>
2020-02-10 11:54:14 -0500

    add route to return meals

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,7 +1,9 @@
 from .auth import RegistrationApi, LoginApi, LogoutApi
+from .meals import MealApi
 
 
 def initialize_routes(api):
     api.add_resource(RegistrationApi, '/api/register')
     api.add_resource(LoginApi, '/api/login')
     api.add_resource(LogoutApi, '/api/logout')
+    api.add_resource(MealApi, '/api/meal')
