742da1fac99e094588384fca7dff95f2ed80c468
Kunal Patel <kunalp1025@gmail.com>
2020-03-05 19:18:15 -0500

    add basic daily meal plan generation

--- backend/services/meal_plan_generator.py
+++ backend/services/meal_plan_generator.py
@@ -11,7 +11,7 @@ def transform_nutrients(nutrients):
 
 def generate_meal_plan(diet):
     # randomly sample meals from mongodb
-    pipeline = [{'$sample': {'size': 300}}]
+    pipeline = [{'$sample': {'size': 100}}]
     meals = Meal.objects().aggregate(*pipeline)
     meals = list(meals)
 
@@ -29,20 +29,33 @@ def generate_meal_plan(diet):
     prob = LpProblem("Daily_Meal_Plan", LpMinimize)
 
     # create meal variables based on meal ids
-    meal_vars = LpVariable.dicts("Meal", meal_ids, lowBound=0, cat='Integer')
+    meal_vars = LpVariable.dicts("Meal", meal_ids, lowBound=0, upBound=1, cat='Integer')
+
+    # create variables that keep track of if a meal has been chosen
+    meals_chosen = LpVariable.dict("chosen", meal_ids, lowBound=0, upBound=1, cat='Integer')
 
     # objective function
     prob += lpSum([meal_dict.get(i)["pricePerServing"] * meal_vars[i] for i in meal_ids])
 
+    # only pick 3 meals
+    # for i in meal_ids:
+    #     #     prob += meal_vars[i] <= meals_chosen[i]*1e5
+    #     # prob += lpSum(meals_chosen.get(i) for i in meal_ids) == 3
+    prob += lpSum(meal_vars[i] for i in meal_ids) == 3
+
     # constraints
     prob += lpSum([meal_dict.get(i)["nutrients"]["Calories"]["amount"] * meal_vars[i] for i in meal_ids]) >= 800.0
 
-    prob += lpSum([meal_dict.get(i)["nutrients"]["Calories"]["amount"] * meal_vars[i] for i in meal_ids]) <= 1300
+    prob += lpSum([meal_dict.get(i)["nutrients"]["Calories"]["amount"] * meal_vars[i] for i in meal_ids]) <= 2400
 
     prob.solve()
 
-    for v in prob.variables():
-        if v.varValue > 0:
-            print(v.name, "=", v.varValue)
+    # for v in prob.variables():
+    #     if v.varValue > 0:
+    #         print(v.name, "=", v.varValue)
 
-    return []
+    for i in meal_ids:
+        if (meal_vars[i].value() > 0):
+            print(i)
+
+    return [meal_dict.get(i) for i in meal_ids if meal_vars[i].value() > 0]
